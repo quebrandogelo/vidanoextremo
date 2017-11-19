453288e36101cad27e0db4e02b5ab72cad53bfb7
Raoni Demarchi <raonidemarchi@gmail.com>
2017-11-19 19:57:30 -0200

    ok

--- js/init.js
+++ js/init.js
@@ -1,18 +1,22 @@
-let parallax	= new M.Parallax(document.querySelector('.parallax'));
 let sidenav 	= new M.Sidenav(document.querySelector('.sidenav'));
 let $content 	= $('#content');
 
-FastClick.attach(document.body);
-
 // SPA
 $('a').click(function(e) {
+	let href = $(this).attr('href');
+	
 	e.preventDefault();
 	
 	sidenav.close();
 	
-	let href = $(this).attr('href');
-	
 	if(href.substr(0, 1) == '#') return;
+	
+	// remove abas na home page
+	$('.nav-content').attr('style', 'display: ' + (href == 'home' ? 'none' : 'block') + ';');
+	
+	// ativa item do menu
+	$('.sidenav li.active').removeClass('active');
+	$(this).parent().addClass('active');
 	
 	$.ajax({
 		method: 'GET',
@@ -24,5 +28,7 @@ $('a').click(function(e) {
 		}
 	});
 });
+
+FastClick.attach(document.body);
 
 $('.sidenav > li:nth-child(2) > a').click();
